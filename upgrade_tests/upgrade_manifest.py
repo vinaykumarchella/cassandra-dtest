1b9256a9e04f9c87c2aec91f8a58808b4eda4a78
Philip Thompson <ptnapoleon@gmail.com>
2016-05-19 14:38:02 -0400

    Revert "git prefix"
    This reverts commit 0a67add26572d09b5bd9fc50807c9b444964dce0.

--- upgrade_tests/upgrade_manifest.py
+++ upgrade_tests/upgrade_manifest.py
@@ -25,11 +25,11 @@ next_2_2_x = None  # None if not yet tagged
 
 indev_3_0_x = VersionMeta(name='indev_3_0_x', variant='indev', version='git:cassandra-3.0', min_proto_v=3, max_proto_v=4)
 current_3_0_x = VersionMeta(name='current_3_0_x', variant='current', version='3.0.5', min_proto_v=3, max_proto_v=4)
-next_3_0_x = VersionMeta(name='next_3_0_x', variant='next', version='git:3.0.6-tentative', min_proto_v=3, max_proto_v=4)
+next_3_0_x = VersionMeta(name='next_3_0_x', variant='next', version='3.0.6-tentative', min_proto_v=3, max_proto_v=4)
 
 indev_3_x = VersionMeta(name='indev_3_x', variant='indev', version='git:cassandra-3.7', min_proto_v=3, max_proto_v=4)
 current_3_x = VersionMeta(name='current_3_x', variant='current', version='3.5', min_proto_v=3, max_proto_v=4)
-next_3_x = VersionMeta(name='next_3_x', variant='next', version='git:3.6-tentative', min_proto_v=3, max_proto_v=4)
+next_3_x = VersionMeta(name='next_3_x', variant='next', version='3.6-tentative', min_proto_v=3, max_proto_v=4)
 
 head_trunk = VersionMeta(name='head_trunk', variant='indev', version='git:trunk', min_proto_v=3, max_proto_v=4)
 
